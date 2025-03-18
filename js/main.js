79e68864dd17d5833bf37060b8ef04ef76a43de6
QadirSalman519 <qadir.softnox@gmail.com>
2025-03-18 20:10:29 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+let navBar = document.querySelector(".nav-link");
+let navCollapse = document.querySelector(".nav-collapse");
+navBar.forEach(function e {
+    e.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+});
